a8506b4034ed9a50241f4b215360b4d1814970f3
Sejan~ <md4sejan@gmail.com>
2023-11-06 04:20:07 +0600

    dynamically change title

--- src/components/layout/MainLayoutes.jsx
+++ src/components/layout/MainLayoutes.jsx
@@ -1,8 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useEffect } from "react";
 
 const MainLayoutes = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.title = `SwapGardens`;
+    } else {
+      document.title = `SwapGardens | ${location.pathname.replace("/", "")}`;
+    }
+
+    if (location.pathname === "/login" && location.state) {
+      document.title = `login`;
+      return;
+    }
+    if (location.state) {
+      document.title = `${location.state}`;
+    }
+  }, [location.pathname, location.state]);
   return (
     <div>
       <Navbar></Navbar>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { ThemeContext } from "../../providers/ThemeProvider";
+import { RiPlantFill } from "react-icons/ri";
 
 const Navbar = () => {
   const { user, logOut } = useContext(ThemeContext);
@@ -72,15 +73,8 @@ const Navbar = () => {
             rel="noopener noreferrer"
             className="flex justify-center lg:justify-start"
           >
-            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-violet-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                fill="currentColor"
-                className="flex-shrink-0 w-5 h-5 rounded-full text-gray-50"
-              >
-                <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
-              </svg>
+            <div className="flex items-center justify-center w-9 h-9 rounded-full bg-violet-600">
+              <RiPlantFill className="text-white text-2xl"></RiPlantFill>
             </div>
             <span className="self-center text-2xl font-semibold ml-2">
               SwapGardens
